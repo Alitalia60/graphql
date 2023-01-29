9505e03fbd91c794abdad65d99f70e76c4d8c288
Andrew Litvinov <lita.private@gmail.com>
2023-01-29 13:12:51 +0300

    fix: change test timeout in package.json

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,23 +1,17 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
-import { graphql, buildSchema } from 'graphql';
 
-// buildSchema(тут заранее нам нужно написать всю типи который требует ТЗ)
-// rootValue(тут все резолверы типа функций который приносит данные по запросу)
-// graphql({schema, source: тут запросы из постмана, rootValue})
-
-// buildSchema()
-
-const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> => {
-
-  fastify.post('/', { schema: { body: graphqlBodySchema, }, },
-
-    async function (request, reply) {
-      const q = request.body.query;
-      reply.header("content-type", "application/json");
-
-      reply.send(graphql())
-    }
+const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
+  fastify
+): Promise<void> => {
+  fastify.post(
+    '/',
+    {
+      schema: {
+        body: graphqlBodySchema,
+      },
+    },
+    async function (request, reply) { }
   );
 };
 
